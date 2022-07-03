7f83a56680d41866eb109a3f53a2d3a2380739f5
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2022-07-04 03:08:01 +0530

    create client mutation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import Header from './components/Header';
 import Clients from './components/Clients';
+import AddClientModal from './components/AddClientModal';
 
 const configuredCache = new InMemoryCache({
   typePolicies: {
@@ -33,6 +34,7 @@ function App() {
       <ApolloProvider client={apolloClient}>
         <Header />
         <div className='container'>
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
